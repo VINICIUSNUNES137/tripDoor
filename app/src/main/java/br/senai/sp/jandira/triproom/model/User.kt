34f79fbd38be0450266ca4a9ddf0b1ae02a4edb4
Vinicius Nunes <viniciusnunes137@gmail.com>
2023-04-11 07:31:13 -0300

    3 activity

---
+++ app/src/main/java/br/senai/sp/jandira/triproom/model/User.kt
@@ -0,0 +1,15 @@
+package br.senai.sp.jandira.triproom.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tbl_user")
+data class User(
+    @PrimaryKey(autoGenerate = true) var id: Long = 0,
+    @ColumnInfo(name = "user-name") var username: String = "",
+    var email: String = "",
+    var phone: String = "",
+    var password: String = "",
+    @ColumnInfo(name = "is_over_18") var isOver18: Boolean = false
+)
